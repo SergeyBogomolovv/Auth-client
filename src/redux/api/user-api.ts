1d2f7576a34c59e7b6287c271f3658f076ff5a12
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-07 10:03:02 +0300

    created aliases in config

---
+++ src/redux/api/user-api.ts
@@ -0,0 +1,15 @@
+import { User } from '@/interfaces/user'
+import { SERVER_URL } from '@/lib/axios'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const userApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: `${SERVER_URL}/users` }),
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query<User, string>({
+      query: (name) => `pokemon/${name}`,
+    }),
+  }),
+})
+
+export const { useGetPokemonByNameQuery } = userApi
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      components: path.resolve(__dirname, './src/components'),
+      hoc: path.resolve(__dirname, './src/hoc'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      interfaces: path.resolve(__dirname, './src/interfaces'),
+      pages: path.resolve(__dirname, './src/pages'),
+      lib: path.resolve(__dirname, './src/lib'),
+      api: path.resolve(__dirname, './src/redux/api'),
+      slices: path.resolve(__dirname, './src/redux/slices'),
+      schemas: path.resolve(__dirname, './src/schemas'),
     },
   },
 })
