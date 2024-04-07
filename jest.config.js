a94b3dcf8fdf0259b6dc3a6bbcd137ed8ef85f17
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-07 16:30:15 +0300

    test environment config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('jest').Config} */
+
+const config = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '^.+\\.css$': 'identity-obj-proxy',
+  },
+}
+
+export default config
